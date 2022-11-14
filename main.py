646364d7bff9e3cb414c42c92ba3bc1984cb10ff
xividi pc <xividi123@gmail.com>
2022-11-14 18:54:53 +0100

    Added open and save file, fixed size changing

--- main.py
+++ main.py
@@ -7,12 +7,35 @@ class GUI(QMainWindow):
         super(GUI, self).__init__()
         uic.loadUi('editor.ui', self)
         self.show()
+        self.plainTextEdit.setFont(QFont('Arial', 12))
 
-        self.setWindowTitle('TextEditor')
+        self.setWindowTitle('Notepad--')
         self.setWindowIcon(QIcon('icon.png'))
+
+        self.action10pt.triggered.connect(lambda: self.change_size(10))
+        self.action12pt.triggered.connect(lambda: self.change_size(12))
+        self.action16pt.triggered.connect(lambda: self.change_size(16))
+        self.action24pt.triggered.connect(lambda: self.change_size(24))
+
+        self.actionOpen.triggered.connect(self.open_file)
+        self.actionSave.triggered.connect(self.save_file)
 
     def change_size(self, size):
         self.plainTextEdit.setFont(QFont('Arial', size))
+
+    def open_file(self):
+        options = QFileDialog.Options()
+        filename, _ = QFileDialog.getOpenFileName(self, 'Open File', '', 'Text Files (*.txt);; Python Files (*.py)', options=options)
+        if filename != '':
+            with open(filename, 'r') as f:
+                self.plainTextEdit.setPlainText(f.read())
+
+    def save_file(self):
+        options = QFileDialog.Options()
+        filename, _ = QFileDialog.getSaveFileName(self, 'Save File', '', 'Text Files (*.txt);;All Files (*)', options=options)
+        if filename != '':
+            with open(filename, 'w') as f:
+                f.write(self.plainTextEdit.toPlainText())
 
 def main():
     app = QApplication([])

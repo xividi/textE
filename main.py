0e0e193178260e15284ed7783faf7c204515a61f
xividi pc <xividi123@gmail.com>
2022-11-14 17:31:07 +0100

    basic gui done

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
+from PyQt5.QtGui import QIcon
 from PyQt5.QtWidgets import *
 from PyQt5 import uic
+
+class GUI(QMainWindow):
+    def __init__(self):
+        super(GUI, self).__init__()
+        uic.loadUi('editor.ui', self)
+        self.show()
+
+        self.setWindowTitle('TextEditor')
+        self.setWindowIcon(QIcon('icon.png'))
+
+def main():
+    app = QApplication([])
+    window = GUI()
+    app.exec_()
+
+if __name__ == '__main__':
+    main()

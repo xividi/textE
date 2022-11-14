9b64e71b94f7c235a733eeb363aeced8e337e212
xividi pc <xividi123@gmail.com>
2022-11-14 20:55:31 +0100

    added font size changer, cleaned up the code

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from PyQt5.QtGui import QIcon, QFont
 from PyQt5.QtWidgets import *
 from PyQt5 import uic
 
+
 class GUI(QMainWindow):
     def __init__(self):
         super(GUI, self).__init__()
@@ -17,20 +18,27 @@ class GUI(QMainWindow):
         self.actionClose.triggered.connect(self.close)
 
         self.actiongetfontsize.triggered.connect(self.get_font_size)
+        self.actionFont_Type.triggered.connect(self.font_type_changer)
 
         self.actionLine_Wrap_Mode.setCheckable(True)
         self.actionLine_Wrap_Mode.triggered.connect(self.line_wrap_mode)
 
+    def font_type_changer(self):
+        font, ok = QFontDialog.getFont()
+        if ok:
+            self.plainTextEdit.setFont(font)
 
     def line_wrap_mode(self):
         if self.actionLine_Wrap_Mode.isChecked():
             self.plainTextEdit.setLineWrapMode(QPlainTextEdit.WidgetWidth)
         else:
             self.plainTextEdit.setLineWrapMode(QPlainTextEdit.NoWrap)
+
     def get_font_size(self):
         size, ok = QInputDialog.getInt(self, 'Font Size', 'Enter font size:', 12, 1, 130, 1)
         if ok:
             self.plainTextEdit.setFont(QFont('Arial', size))
+            return size
 
     def open_file(self):
         options = QFileDialog.Options()
@@ -55,17 +63,20 @@ class GUI(QMainWindow):
         reply.addButton(QPushButton("Don't save"), QMessageBox.NoRole)
         reply.addButton(QPushButton('Cancel'), QMessageBox.RejectRole)
 
-        anwser = reply.exec_()
+        answer = reply.exec_()
 
-        if anwser == 0:
+        if answer == 0:
             self.save_file()
             event.accept()
-        elif anwser == 2:
+        elif answer == 2:
             event.ignore()
+
+
 def main():
     app = QApplication([])
     window = GUI()
     app.exec_()
+
 
 if __name__ == '__main__':
     main()
